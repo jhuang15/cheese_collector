a0782fc0da91f4efdcf6bc3c55e62d20c420ece0
jhuang15 <jchuang1020@gmail.com>
2022-05-06 17:16:04 -0400

    Complete Part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+class Cheese:  
+  def __init__(self, name, type, description, age):
+    self.name = name
+    self.type = type
+    self.description = description
+    self.age = age
+
+cheeses = [
+  Cheese('Parmigiano-Reggiano', 'hard', 'Italian hard, granular cheese produced from cow''s milk', 12),
+  Cheese('Mozzarella', 'fresh', 'southern Italian cheese traditionally made from Italian buffalo''s milk by the pasta filata method', 0),
+  Cheese('Camembert', 'soft white rind', 'a moist, soft, creamy, surface-ripened cow''s milk cheese', 1)
+]
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Cheesey!! :D</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def cheeses_index(request):
+  return render(request, 'cheeses/index.html', { 'cheeses': cheeses })
