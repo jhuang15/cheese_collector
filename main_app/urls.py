b0c437a34fe2c9c61c11eb7b546269fbcb7c158b
jhuang15 <jchuang1020@gmail.com>
2022-05-09 16:13:33 -0400

    Part 2

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('cheeses/', views.cheeses_index, name='index')
+  path('cheeses/', views.cheeses_index, name='index'),
+  path('cheeses/<int:cheese_id>/', views.cheeses_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,28 +1,19 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
-
-
-class Cheese:  
-  def __init__(self, name, type, description, age):
-    self.name = name
-    self.type = type
-    self.description = description
-    self.age = age
-
-cheeses = [
-  Cheese('Parmigiano-Reggiano', 'hard', 'Italian hard, granular cheese produced from cow''s milk', 12),
-  Cheese('Mozzarella', 'fresh', 'southern Italian cheese traditionally made from Italian buffalo''s milk by the pasta filata method', 0),
-  Cheese('Camembert', 'soft white rind', 'a moist, soft, creamy, surface-ripened cow''s milk cheese', 1)
-]
+from .models import Cheese
 
 # Define the home view
 def home(request):
-  #return HttpResponse('<h1>Cheesey!! :D</h1>')
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def cheeses_index(request):
-  return render(request, 'cheeses/index.html', { 'cheeses': cheeses })
+  cheeses = Cheese.objects.all()
+  
+  return render(request, 'cheeses/index.html', { 
+    'cheeses': cheeses })
+
+def cheeses_detail(request, cheese_id):
+  cheese = Cheese.objects.get(id=cheese_id)
+  return render(request, 'cheeses/detail.html', { 'cheese': cheese })
